1acfb7540883023cc828ca3ace006ee80ff6735a
drumstrum88 <rev.jsanderson@gmail.com>
2023-05-27 13:25:48 -0500

    adding all the things

--- events/domEvents.js
+++ events/domEvents.js
@@ -1,3 +1,15 @@
+import { showBooks } from '../pages/books';
+import {
+  deleteBook, getBooks, getSingleBook,
+} from '../api/bookData';
+import addBookForm from '../components/forms/addBookForm';
+import addAuthorForm from '../components/forms/addAuthorForm';
+import getBookDetails from '../api/mergedData';
+// eslint-disable-next-line import/named
+import viewBook from '../pages/viewBook';
+import { getAuthors, getSingleAuthor } from '../api/authorData';
+import { viewAuthorDetails } from '../pages/viewAuthor';
+
 const domEvents = () => {
   document.querySelector('#main-container').addEventListener('click', (e) => {
     // TODO: CLICK EVENT FOR DELETING A BOOK
@@ -5,40 +17,62 @@ const domEvents = () => {
       // eslint-disable-next-line no-alert
       if (window.confirm('Want to delete?')) {
         console.warn('CLICKED DELETE BOOK', e.target.id);
-        console.warn(e.target.id.split('--'));
+        const [, fireBaseKey] = e.target.id.split('--');
+
+        deleteBook(fireBaseKey).then(() => {
+          getBooks().then(showBooks);
+        });
       }
     }
 
     // TODO: CLICK EVENT FOR SHOWING FORM FOR ADDING A BOOK
     if (e.target.id.includes('add-book-btn')) {
-      console.warn('ADD BOOK');
+      addBookForm();
     }
 
     // TODO: CLICK EVENT EDITING/UPDATING A BOOK
     if (e.target.id.includes('edit-book-btn')) {
-      console.warn('EDIT BOOK', e.target.id);
-      console.warn(e.target.id.split('--'));
+      const [, firebaseKey] = e.target.id.split('--');
+
+      getSingleBook(firebaseKey).then((bookObj) => addBookForm(bookObj));
     }
     // TODO: CLICK EVENT FOR VIEW BOOK DETAILS
     if (e.target.id.includes('view-book-btn')) {
-      console.warn('VIEW BOOK', e.target.id);
-      console.warn(e.target.id.split('--'));
+      const [, firebaseKey] = e.target.id.split('--');
+
+      getBookDetails(firebaseKey).then(viewBook);
     }
 
     // FIXME: ADD CLICK EVENT FOR DELETING AN AUTHOR
     if (e.target.id.includes('delete-author-btn')) {
       // eslint-disable-next-line no-alert
       if (window.confirm('Want to delete?')) {
-        console.warn('DELETE AUTHOR', e.target.id);
-        console.warn(e.target.id.split('--'));
+        console.warn('CLICKED DELETE AUTHOR', e.target.id);
+        const [, fireBaseKey] = e.target.id.split('--');
+
+        deleteBook(fireBaseKey).then(() => {
+          getAuthors().then(showBooks);
+        });
       }
     }
 
     // FIXME: ADD CLICK EVENT FOR SHOWING FORM FOR ADDING AN AUTHOR
     if (e.target.id.includes('add-author-btn')) {
-      console.warn('ADD AUTHOR');
+      addAuthorForm();
     }
+    // FIXME: add click event for viewing an author details
+    if (e.target.id.includes('view-author-btn')) {
+      const [, firebaseKey] = e.target.id.split('--');
+
+      viewAuthorDetails(firebaseKey);
+    }
+
     // FIXME: ADD CLICK EVENT FOR EDITING AN AUTHOR
+    if (e.target.id.includes('update-author--')) {
+      const [, firebaseKey] = e.target.id.split('--');
+
+      getSingleAuthor(firebaseKey).then((authorObj) => addAuthorForm(authorObj));
+    }
   });
 };
 
---
+++ pages/viewAuthor.js
@@ -0,0 +1,40 @@
+import clearDom from '../utils/clearDom';
+import renderToDom from '../utils/renderToDom';
+import { getSingleAuthor, getAuthorBooks } from '../api/authorData';
+
+const renderAuthorDetails = (obj) => {
+  clearDom();
+  const domString = `
+    <div class="mt-5 d-flex flex-wrap" id="author-details-container">
+      <div class="mt-5 d-flex flex-wrap">
+            <i id="edit-author-btn--${obj.firebaseKey}" class="fas fa-edit btn btn-info"></i>
+            <i id="delete-author--${obj.firebaseKey}" class="btn btn-danger fas fa-trash-alt"></i>
+          </div>
+        </div>
+        <div class="text-white ms-5 details">
+          <h5>${obj.first_name}</h5>
+          Author Email: <a href="mailto:${obj.email}">${obj.email}</a>
+          <h4>Favorite Books:</h4>
+          <ul>
+            ${obj.favoriteBooks ? obj.favoriteBooks.map((book) => `<li>${book}</li>`).join('') : ''}
+          </ul>
+          <hr>
+        </div>
+      </div>
+    </div>
+  `;
+  renderToDom('#author-details-container', domString);
+};
+
+const viewAuthorDetails = (firebaseKey) => {
+  getSingleAuthor(firebaseKey)
+    .then((author) => getAuthorBooks(firebaseKey).then((books) => {
+      // eslint-disable-next-line no-param-reassign
+      author.favoriteBooks = books.map((book) => book.title);
+      renderAuthorDetails(author);
+    }))
+    .catch((err) => console.warn(err));
+};
+
+export default renderAuthorDetails;
+export { viewAuthorDetails };
